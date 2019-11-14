4320abdfe49ce1d2af06376481e7d3abee2a5a0a
Dmitriy Garus <dimitrobase@gmail.com>
2019-11-14 20:55:49 +0200

    master1

---
+++ RF_autotest/Clients/BaseClient.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RF_autotest.Clients
+{
+    class BaseClient
+    {
+        private String _mainResource=""; //todo
+        private RestClient _client;
+
+        protected BaseClient()
+        {
+            _client = new RestClient(_mainResource);
+        }
+
+        IRestResponse SendRequest(RestRequest request)
+        {
+            var response =_client.Execute(request);
+            return response;
+        }
+    }
+}
