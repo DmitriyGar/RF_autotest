b5b014a9e156330e97ae8c0d09b02966a39acdc4
DmitriyGar <dimitrobase@gmail.com>
2023-11-15 15:35:59 +0200

    Update Configuration.cs

--- RF_autotest/Settings/Configuration.cs
+++ RF_autotest/Settings/Configuration.cs
@@ -8,17 +8,17 @@ namespace RF_autotest.Settings
 {
     public static class Configuration
     {
-        public static readonly string UrlQaEnvironment= "http://rf-qa-app1.zkpsl3bk22wuvfspx0uhovklae.bx.internal.cloudapp.net";
-        public static readonly string UrlIntEnvironment = "http://cbd-int-gateway1.zkpsl3bk22wuvfspx0uhovklae.bx.internal.cloudapp.net";
-        public static readonly string HostQaDataBaseEnvironment = "rf-qa-db1.zkpsl3bk22wuvfspx0uhovklae.bx.internal.cloudapp.net";
-        public static readonly string HostINTDataBaseEnvironment = "rf-qa-db1.zkpsl3bk22wuvfspx0uhovklae.bx.internal.cloudapp.net";
+        public static readonly string UrlQaEnvironment= "http://rf-qa-app1.cloudapp.net";
+        public static readonly string UrlIntEnvironment = "http://cbd-int-gateway1.cloudapp.net";
+        public static readonly string HostQaDataBaseEnvironment = "rf-qa-db1.cloudapp.net";
+        public static readonly string HostINTDataBaseEnvironment = "rf-qa-db1.cloudapp.net";
         public static readonly string Port8080 = "8080";
-        public static readonly string LoginRfAnalyst = "rf_analyst";
-        public static readonly string NameRfAnalyst = "rf_analyst@cis-cust.lan";
-        public static readonly string LoginRfManager = "rf_manager";
-        public static readonly string LoginRfAdmin = "rf_admin";
+        public static readonly string LoginRfAnalyst = "analyst1";
+        public static readonly string NameRfAnalyst = "analyst@cis-cust.lan";
+        public static readonly string LoginRfManager = "manager";
+        public static readonly string LoginRfAdmin = "admin";
         public static readonly string Client = "umbrella";
-        public static readonly string Password = "Dummy#123";
+        public static readonly string Password = "dummy#123";
               
     }
 }
